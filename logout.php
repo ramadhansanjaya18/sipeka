341b57ea0a3f59e591d09b544833c0172742f954
ramadhansanjaya18 <ramadhansanjaya18@gmail.com>
2025-12-05 08:42:49 +0700

    edit by rama

--- logout.php
+++ logout.php
@@ -1,18 +1,11 @@
 <?php
-/**
- * Skrip Logout Pengguna.
- *
- * File ini bertanggung jawab untuk mengakhiri session pengguna.
- * Ia akan menghapus semua data session dan mengalihkan pengguna
- * kembali ke halaman login dengan sebuah pesan notifikasi.
- */
 
 // 1. Selalu mulai session untuk dapat memanipulasinya.
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 
-// 2. Hapus semua variabel session dan hancurkan session itu sendiri.
+
 session_unset();
 session_destroy();
 
